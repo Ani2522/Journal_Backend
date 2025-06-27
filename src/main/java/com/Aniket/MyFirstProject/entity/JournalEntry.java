dbd4395646497fcf051b321336a27b0ab3b080d1
aniket-metron <aniket.jain@metronlabs.com>
2025-06-27 17:08:40 +0530

    Added Authentication to Journal Entry

---
+++ src/main/java/com/Aniket/MyFirstProject/entity/JournalEntry.java
@@ -0,0 +1,25 @@
+package com.Aniket.MyFirstProject.entity;
+
+import lombok.*;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Document(collection = "journal_entries")
+@Data
+@NoArgsConstructor //this is requied to deinitializarion that json to pojo
+public class JournalEntry {
+    @Id
+    private ObjectId id;
+    @NonNull
+    private String title;
+    private String content;
+
+    private LocalDateTime date;
+
+
+
+}
---
+++ src/main/java/com/Aniket/MyFirstProject/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.Aniket.MyFirstProject.repository;
+
+import com.Aniket.MyFirstProject.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    User findByUserName(String userName);
+
+    void deleteByUserName(String username);
+}
